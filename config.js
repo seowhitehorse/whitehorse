b0c05c9ac84148e730d0c13034a86722057981cc
seowhitehorse <seowhitehorse@gmail.com>
2026-06-05 10:25:40 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // ⚠️ DO NOT PUSH THIS FILE TO GITHUB
 // This file is listed in .gitignore
-window.GEMINI_API_KEY = 'AQ.Ab8RN6IqGlLb4Z5SV8903jXOl48_YFKuq5xCss6ATELiYkXeQQ';
+window.GEMINI_API_KEY = 'AIzaSyBbCl-Yelu0TEjy7hvNj58Hj8-REuC8qe4';
